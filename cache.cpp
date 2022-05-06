f8cf90243e94ef0448213f6cb63d325d386414c9
Tobi-D7 <tobid7@outlook.de>
2022-05-06 22:59:01 +0200

    Fix

--- cache.cpp
+++ cache.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <cstdint>
 #include "utils.hpp"
+#include "TitleManager.hpp"
 
 Log cachelog;
 
@@ -18,7 +19,7 @@ void Cache::Create(std::vector<std::shared_ptr<Title>> t, const std::string& pat
     {
         RenderD7::Msg::DisplayWithProgress("D7-Menu-Core", "Writing Cache: " + t[i]->name(), i, (int)t.size(), RenderD7::Color::Hex("#00DD11"));
         cachedata[t[i]->name()]["name"] = t[i]->name();
-        //cachedata[t[i]->name()]["author"] = t[i]->author();
+        cachedata[t[i]->name()]["author"] = t[i]->author();
         cachedata[t[i]->name()]["prod"] = t[i]->prodcode();
         cachedata[t[i]->name()]["id"] = std::to_string(t[i]->ID());
     }
@@ -53,14 +54,20 @@ bool Cache::Read(std::vector<std::shared_ptr<Title>> t, const std::string& path,
 
         std::string prodCode = cachedata[secs[i]]["prod"];
         
-
+        std::string __author__ = cachedata[secs[i]]["author"];
         uint64_t newID = 0;
         std::istringstream iss(cachedata[secs[i]]["id"]);
         iss >> newID;
         RenderD7::Msg::DisplayWithProgress("D7-Menu-Core",  "Loading Titles from cache: ", i, secs.size(), RenderD7::Color::Hex("#00DD11"));
-        newData->LoadFromCache(newID, title, prodCode, nand ? MEDIATYPE_NAND : MEDIATYPE_SD);
+        newData->LoadFromCache(newID, title, __author__, prodCode, nand ? MEDIATYPE_NAND : MEDIATYPE_SD);
         t.push_back(newData);
     }
+    for (size_t f = 0; f < t.size(); f++)
+    {
+        TitleManager::sdtitles.push_back(t[f]);
+    }
     return true;
+    
+    
     cachelog.Write("return");
 }
